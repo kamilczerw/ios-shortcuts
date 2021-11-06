84667cc9904905a368364744ddcee9976987d4bb
Kamil Czerwiński <kamil@czerwinski.dev>
2021-11-06 10:35:26 +0100

    add more

--- Time-reporting/add_time.py
+++ Time-reporting/add_time.py
@@ -1,2 +1,18 @@
 import dialogs
 import sys
+import console
+import webbrowser
+
+def main():
+	console.clear()
+	hours = dialogs.input_alert('Worked hours', 'hours', '08:00')
+	#print(hours)
+	callback = sys.argv[-1] + '?x-source=Pythonista3&hours=' + hours
+	
+	print(callback)
+	
+	webbrowser.open(callback)
+	
+
+if __name__ == '__main__':
+	main()
